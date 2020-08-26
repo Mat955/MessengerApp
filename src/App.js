23c3e4eeec1b95028817db5c6365518b123d663f
Mateusz Lewartowski <lewar2013c@gmail.com>
2020-08-26 19:09:29 +0200

    sidebar and sidebarchat component with style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <h1>build1</h1>
 
       <div className="app__body">
-        {/*Sidebar*/}
+        <Sidebar />
         {/*Chat*/}
       </div>
     </div>
