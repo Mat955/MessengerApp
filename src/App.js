c1b47c001d14e8b7f37bd25b33752a5d293e90ec
Mateusz Lewartowski <lewar2013c@gmail.com>
2020-08-26 17:50:06 +0200

    git init project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="app">
+      <h1>build1</h1>
+
+      <div className="app__body">
+        {/*Sidebar*/}
+        {/*Chat*/}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD5tGKTZSdD7Z5AvXKf1wqNfF1WM60QqBc",
+  authDomain: "messengerapp95.firebaseapp.com",
+  databaseURL: "https://messengerapp95.firebaseio.com",
+  projectId: "messengerapp95",
+  storageBucket: "messengerapp95.appspot.com",
+  messagingSenderId: "71442133242",
+  appId: "1:71442133242:web:67035364236dc6c4d25943",
+  measurementId: "G-623XE4DVH1"
+};
