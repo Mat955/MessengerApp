a2d8d8fb40db6038da5b9a75a6045acfceeb5574
Mateusz Lewartowski <lewar2013c@gmail.com>
2021-05-05 14:39:18 +0200

    added finished app

---
+++ build/precache-manifest.7a95bc914e01b966cce31610c0aec700.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0385f960d2de5569706a3f4c400c9b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee8b9f475fe100cdd9a3",
+    "url": "/static/css/main.d0c6c8d7.chunk.css"
+  },
+  {
+    "revision": "61969b16aae53c9de225",
+    "url": "/static/js/2.c94d4776.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.c94d4776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee8b9f475fe100cdd9a3",
+    "url": "/static/js/main.f9f04ca3.chunk.js"
+  },
+  {
+    "revision": "17db2cbcbaf868372d0a",
+    "url": "/static/js/runtime-main.7a6f9807.js"
+  }
+]);
