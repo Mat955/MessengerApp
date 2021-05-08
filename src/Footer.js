305ac332398588dbac040eba42950e9bd50111ab
Mateusz Lewartowski <lewar2013c@gmail.com>
2021-05-08 12:38:02 +0200

    added site

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>Wykonanie: <a href="https://ematpro.dev">eMATPRO.dev</a></p>
+    </div>
+  )
+}
+
+export default Footer
